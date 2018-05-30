aa4023c035ac964da0ff3d5467d22dfb0103a2a7
tim <timvandemerwe@gmail.com>
2018-05-30 17:31:45 +0200

    fix voor nick

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,11 +4,18 @@ import { AuthData } from '../../providers/auth-data';
 import { Login } from '../login/login';
 import * as firebase from 'firebase'
 
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+    tab1Root = HomePage;
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
 
     constructor(public navCtrl: NavController, public authData: AuthData) {
 
