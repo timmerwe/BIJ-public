97d516ff528102e8474d9634dcb3487bdea19ea8
tim <timvandemerwe@gmail.com>
2018-05-31 15:41:56 +0200

    change info gebruikers page

--- src/assets/js/myscripts.js
+++ src/assets/js/myscripts.js
@@ -1,7 +1,11 @@
-function hoi1() {
+function changeInfo() {
     document.getElementById("main-info").innerHTML = "<p>Uitleg over hoe leerkrachten en ouders/verzorgers de APP kunnen downloaden en gebruiken. Dit willen we doen d.m.v. 2 codes. 1 t.b.v. leerkrachten (interne gebruikers) en 1 t.b.v. ouders/verzorgers (externe gebruikers)</p>";
+    document.getElementById("algemeen").className = "border-bottom";
+    document.getElementById("overzicht").className = "noBorder-bottom";
   }
 
-  function hoi2() {
+  function changeInfo2() {
     document.getElementById("main-info").innerHTML = "Overzicht";
+    document.getElementById("overzicht").className = "border-bottom";
+    document.getElementById("algemeen").className = "noBorder-bottom";
   }
